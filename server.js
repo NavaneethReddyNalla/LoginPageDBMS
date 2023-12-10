0d3b1c1d1b2e10cc5ce1a0d254fe02876d0650df
Navaneeth <navaneethreddynalla@gmail.com>
2023-12-10 14:45:37 +0530

    Tested Express Server

--- server.js
+++ server.js
@@ -2,10 +2,50 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+let users = { Navaneeth: "123456" };
+
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.status(200).sendFile(__dirname + "/public/loginpage.html");
+  res.status(200).redirect("/loginpage.html");
+});
+
+app.get("/users/:name", (req, res) => {
+  const name = req.params.name;
+  console.log(name);
+
+  if (name in users) {
+    res.status(200).send(JSON.stringify(users));
+  } else {
+    res.status(404).redirect("/");
+  }
+});
+
+app.post("/", (req, res) => {
+  const user = req.body.username;
+  const pwd = req.body.password;
+
+  if (user in users && users[user] === pwd) {
+    res.redirect(`/users/${user}`);
+  } else {
+    console.log("Invalid!");
+    res.redirect("/");
+  }
+});
+
+app.post("/createuser", (req, res) => {
+  const user = req.body.username;
+  const pwd = req.body.password;
+  const email = req.body.email;
+
+  if (!(user in users)) {
+    users[user] = pwd;
+    res.redirect("/");
+  } else {
+    console.log("Username taken");
+    res.redirect("/signup.html");
+  }
 });
 
 app.listen(PORT, () => {
